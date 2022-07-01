9a1a9c4d155e2bb21793b29266fffcf621e04863
Liam Cammpbell <liam22campbell@gmail.com>
2022-07-01 08:56:26 -0700

    Add fetch API call to money-service.js.

--- src/js/money-service.js
+++ src/js/money-service.js
@@ -1,2 +1,15 @@
-
-const url = `https://v6.exchangerate-api.com/v6/{process.env.API_KEY}/latest/USD`
+export default class MoneyService {
+  static getExchange() {
+    return fetch(`https://v6.exchangerate-api.com/v6/{process.env.API_KEY}/latest/USD`)
+    .then (function(response) {
+      if (!response.ok) {
+        throw Error(response.error-type);
+      }
+      return response.json();
+    })
+    .catch(function(error) {
+      return error;
+    })
+    
+  }
+}
