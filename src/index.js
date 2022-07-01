4ef2a6597d470272f652f0cca8752b6f9782fde5
Liam Cammpbell <liam22campbell@gmail.com>
2022-07-01 11:14:34 -0700

    Remove .text error message from document.ready.

--- src/index.js
+++ src/index.js
@@ -9,13 +9,12 @@ function getMoney(response) {
     console.log(response);
     $('.showRate').text(`${response.conversion_rates.USD}`);
   } else {
-    $('.showErrors').text(`There was an error: ${response.error_type}`);
+    $('.showErrors').text(`There was an error: ${response}`);
   }
 }
 $(document).ready(function () {
   $('#submit-btn').click(function () {
     let currency = $('#convert-to').val();
-    $('.showErrors').text(`There was an error:`);
     console.log(currency);
     MoneyService.getExchange(currency).then(function (response) {
       getMoney(response);
