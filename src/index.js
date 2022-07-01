b3e391a18d9d50d5e09cc8989bfbd5e13fce8442
Liam Cammpbell <liam22campbell@gmail.com>
2022-07-01 16:56:51 -0700

    Hides and shows Error messages appropiately.

--- src/index.js
+++ src/index.js
@@ -4,43 +4,39 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 import MoneyService from './js/money-service.js';
 
-// async function makeApiCall(currency) {
-//   const response = await MoneyService.getExchange(currency);
-//   //let dollarAmount = $('#amount-usd').val(); 
-//   getMoney(response);
-
-// }
-
 function getMoney(response) {
-  if (response) {
+  if (response.result === "success") {
     let exchangeRate = `${1/response.conversion_rates.USD}`;
     return exchangeRate;
   } else {
     $('.showErrors').text(`There was an error: ${response.error}`);
+
   }
 }
-
-// $(document).ready(function() {
-//   $('#submit').click(function () {
-//     let currency = $('#converty-to').val();
-//     //let dollarAmount = $('#amount-usd').val(); 
-//     //clearFields();
-//     makeApiCall(currency);
-
-//   });
-// });
-
-
 
 function usMoney(exchangeRate) {
   let dollars = $('#amount-usd').val();
   let foreignMoney = exchangeRate * dollars;
+  currencyVailidator(foreignMoney);
   return foreignMoney;
 }
 
+function currencyVailidator (foreignMoney) {
+  if (isNaN(foreignMoney)) {
+    $('.showExchange').hide();
+    $('.showRate').hide();
+    $('.errorMessage').text(`We are not familiar with that currency or format. Please reenter in the format of three uppercase letters, like so : USD`)
+    $('.errorMessage').show();
+    $('.showErrors').show();
+  }
+}
 
 $(document).ready(function () {
   $('#submit').click(function () {
+    $('.showExchange').show();
+    $('.showRate').show();
+    $('.errorMessage').hide();
+    $('.showErrors').hide();
     let currency = $('#convert-to').val();
     MoneyService.getExchange(currency)
     .then(function (response) {
@@ -48,6 +44,7 @@ $(document).ready(function () {
       let foreignMoney = usMoney(exchangeRate);
       $('.showExchange').text("You have " + foreignMoney.toFixed(2) + " in " + currency)
       $('.showRate').text("The echange rate is : " + exchangeRate + " for " + currency)
+      
     });
    
   });
