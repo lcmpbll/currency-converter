e60112196a6f73c6392b226d955004b5eed6cca3
Liam Cammpbell <liam22campbell@gmail.com>
2022-07-01 14:36:06 -0700

    Exchange rate returned from A PI.

--- src/index.js
+++ src/index.js
@@ -4,14 +4,34 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 import MoneyService from './js/money-service.js';
 
+// async function makeApiCall(currency) {
+//   const response = await MoneyService.getExchange(currency);
+//   //let dollarAmount = $('#amount-usd').val(); 
+//   getMoney(response);
+
+// }
+
 function getMoney(response) {
   if (response) {
     let exchangeRate = `${1/response.conversion_rates.USD}`;
+    console.log(exchangeRate);
     return exchangeRate;
   } else {
-    $('.showErrors').text(`There was an error: ${response.error_message}`);
+    $('.showErrors').text(`There was an error: ${response.error}`);
   }
 }
+
+// $(document).ready(function() {
+//   $('#submit').click(function () {
+//     let currency = $('#converty-to').val();
+//     //let dollarAmount = $('#amount-usd').val(); 
+//     //clearFields();
+//     makeApiCall(currency);
+
+//   });
+// });
+
+
 
 // function usMoney(exchangeRate, dollars) {
 //   let foreignMoney = exchangeRate * dollars;
@@ -21,13 +41,14 @@ function getMoney(response) {
 
 
 $(document).ready(function () {
-  $('#submit-btn').click(function () {
+  $('#submit').click(function () {
     let currency = $('#convert-to').val();
-   // let dollars = $('#amount-usd').val();
+    //let dollars = $('#amount-usd').val();
     MoneyService.getExchange(currency)
     .then(function (response) {
-      let exchangeRate = getMoney(response);
-      console.log(exchangeRate);
+      getMoney(response);
+      //console.log(exchangeRate)
+
     });
    
   });
