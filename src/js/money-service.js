2845a99fb09b4c406d637f60adc42a423b33b03a
Liam Cammpbell <liam22campbell@gmail.com>
2022-07-01 14:13:33 -0700

    WIP,

--- src/js/money-service.js
+++ src/js/money-service.js
@@ -1,9 +1,23 @@
+// export default class MoneyService {
+//   static async getExchange(currency) {
+//     try {
+//     const response = await fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/${currency}`);
+//       if (!response.ok) {
+//         throw Error(response.error_message);
+//       }
+//       return response.json();
+//     } catch(error) {
+//       return error;
+//     } 
+//   }
+// }
+
 export default class MoneyService {
   static getExchange() {
     return fetch(`https://v6.exchangerate-api.com/v6/{process.env.API_KEY}/latest/USD`)
     .then (function(response) {
       if (!response.ok) {
-        throw Error(response.error-type);
+        throw Error(response);
       }
       return response.json();
     })
