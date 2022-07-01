d5d66196a0a0759f6d94d621973f83716c9d1c3a
Liam Cammpbell <liam22campbell@gmail.com>
2022-07-01 09:08:59 -0700

    Add UI logic with a fetch function to retieve and display API call.

--- src/index.js
+++ src/index.js
@@ -3,10 +3,21 @@
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
+import MoneyService from './js/money-service.js'
 
-$(document).ready(function () {
-  $('selector').submit(function (event)) {
-    event.preventDefault(); //if it's a form
-    // UI code here
+function getMoney (response) {
+  if(response.conversion_rates) {
+    $('.showRate').text(`${response.conversion_rates.AUD}`);
+  } else {
+    $('.showErrors').text(`There was an error: ${response.error_type}`;)
   }
+}
+$(document).ready(function () {
+  $('#submit-btn').submit(function (event) {
+    event.preventDefault(); 
+    MoneyService.getExchange();
+      .then(function(response) {
+        getMoney(response);
+      });
+  });
 });
