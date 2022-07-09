3cc10dc13656b81094106770d2fffd1ef8c986ef
Liam Cammpbell <liam22campbell@gmail.com>
2022-07-08 20:15:11 -0700

    Fix formatting.

--- src/js/money-service.js
+++ src/js/money-service.js
@@ -3,15 +3,15 @@
 export default class MoneyService {
   static getExchange(currency) {
     return fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/${currency}`)
-    .then (function(response) {
-      if (!response.ok) {
-        throw Error(response);
-      } 
-      return response.json();
-    })
-    .catch(function(response) {
-      return response;
-    })
+      .then (function(response) {
+        if (!response.ok) {
+          throw Error(response);
+        } 
+        return response.json();
+      })
+      .catch(function(response) {
+        return response;
+      });
     
   }
 }
