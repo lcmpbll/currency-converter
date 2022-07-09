894409df57159ff596a59bbb3f2f87536740ffaa
Liam Cammpbell <liam22campbell@gmail.com>
2022-07-08 20:59:10 -0700

    Change error to functioning message

--- src/js/money-service.js
+++ src/js/money-service.js
@@ -5,12 +5,12 @@ export default class MoneyService {
     return fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/${currency}`)
       .then (function(response) {
         if (!response.ok) {
-          throw Error(response);
+          throw Error(response.status);
         } 
         return response.json();
       })
-      .catch(function(response) {
-        return response;
+      .catch(function(error) {
+        return error.message;
       });
     
   }
